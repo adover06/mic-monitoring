a2bb952d37bccd51e318b583ab663f7a0984da82
Andrew Dover <monkeypunk811@gmail.com>
2026-03-02 13:21:04 -0800

    API Key header

--- src/server.py
+++ src/server.py
@@ -2,13 +2,20 @@ from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import FileResponse
+from fastapi.security import APIKeyHeader
 import csv
 import os
 from datetime import datetime
+import dotenv
+
+dotenv.load_dotenv()
 
 
 app = FastAPI()
 templates = Jinja2Templates(directory="static")
+api_key_header_scheme = APIKeyHeader(name="X-API-Key", auto_error=True)
+
+VALID_API_KEY = os.getenv("API_KEY")
 CSV_FILE = "data/noise_data.csv"
 
 # Ensure CSV exists
@@ -16,6 +23,15 @@ if not os.path.exists(CSV_FILE):
     with open(CSV_FILE, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["Timestamp", "Noise_Level"])
+
+
+async def get_api_key(x_api_key: Annotated[str, Depends(api_key_header_scheme)]):
+    if x_api_key != VALID_API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key"
+        )
+    return x_api_key
 
 @app.get("/health")
 async def health_check():
@@ -26,7 +42,7 @@ async def health_check():
         "storage_size": f"{os.path.getsize(CSV_FILE) / 1024:.2f} KB"
     }
 
-@app.post("/log")
+@app.post("/log", dependencies=[Depends(get_api_key)])
 async def log_noise(level: int = Form(...)):
     timestamp = datetime.now().strftime("%H:%M:%S")
     with open(CSV_FILE, "a", newline="") as f:
